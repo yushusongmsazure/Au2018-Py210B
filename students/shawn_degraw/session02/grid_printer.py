dade6774f809f06bc634810f7440583a5fec24f9
motians <motians@comcast.net>
2018-10-06 12:31:53 -0700

    Part 3 multiple box grid implemented

--- students/shawn_degraw/session02/grid_printer.py
+++ students/shawn_degraw/session02/grid_printer.py
@@ -16,9 +16,33 @@ def print_grid(n):
         print("|" + n * " " + "|" + n * " " + "|")
     print("+" + n * "-" + "+" + n * "-" + "+")
 
+def print_grid2(x , y):
+
+    for i in range(x):
+        
+        #First line print
+        print(end="+")
+        for i in range(x):
+            print(y * "-", end="+")
+        print()
+
+        #body print
+        for i in range(y):
+            print(end="|")
+            for i in range(x):
+                print(+ y * " ", end="|")
+            print()
+    
+    #last line print
+    print(end="+")
+    for i in range(x):
+        print(y * "-", end="+")
+
 
 
 if __name__ == "__main__":
     basicprintgrid()
     print()
     print_grid(4)
+    print()
+    print_grid2(6,6)
