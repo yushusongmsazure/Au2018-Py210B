0c8151b5d8e64b8fff17764df3f609e181ec82d1
motians <motians@comcast.net>
2018-10-06 09:00:40 -0700

    Part 1 basic grid print

---
+++ students/shawn_degraw/session02/grid_printer.py
@@ -0,0 +1,12 @@
+def basicprintgrid():
+    print("+" + "----" + "+" + "----" + "+")
+    for i in range(4):
+        print("|" + "    " + "|" + "    " + "|")
+    print("+" + "----" + "+" + "----" + "+")
+    for i in range(4):
+        print("|" + "    " + "|" + "    " + "|")
+    print("+" + "----" + "+" + "----" + "+")    
+
+if __name__ == "__main__":
+    basicprintgrid()
+
