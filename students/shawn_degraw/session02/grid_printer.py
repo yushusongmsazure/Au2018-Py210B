46073710ab59d7d6fa7fa6e8cb2d47cd268dbf6b
motians <motians@comcast.net>
2018-10-06 09:22:35 -0700

    Part 2 pass grid size to new function

--- students/shawn_degraw/session02/grid_printer.py
+++ students/shawn_degraw/session02/grid_printer.py
@@ -7,6 +7,18 @@ def basicprintgrid():
         print("|" + "    " + "|" + "    " + "|")
     print("+" + "----" + "+" + "----" + "+")    
 
+def print_grid(n):
+    print("+" + n * "-" + "+" + n * "-" + "+")
+    for i in range(n):
+        print("|" + n * " " + "|" + n* " " + "|")
+    print("+" + n * "-" + "+" + n * "-" + "+")
+    for i in range(n):
+        print("|" + n * " " + "|" + n * " " + "|")
+    print("+" + n * "-" + "+" + n * "-" + "+")
+
+
+
 if __name__ == "__main__":
     basicprintgrid()
-
+    print()
+    print_grid(4)
