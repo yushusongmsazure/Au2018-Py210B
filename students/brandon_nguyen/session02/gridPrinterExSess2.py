07e65c29254222fe16af486ea8547b6d9b082431
Brandon Nguyen <bnguyen@Brandons-iMac.local>
2018-10-10 16:33:57 -0700

    add new gridPrinter file

---
+++ students/brandon_nguyen/session02/gridPrinterExSess2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#Grid Printer Excercise in session02 Au2018-Py210B
+plus    = '+'
+minus   = ' -'
+bar     = '|'
+space   = ' '
+barSpaces = 9
+print(plus+minus*4+' '+plus+minus*4+ ' '+plus)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(plus+minus*4+' '+plus+minus*4+ ' '+plus)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(bar+space*barSpaces+bar+space*barSpaces+bar)
+print(plus+minus*4+' '+plus+minus*4+ ' '+plus)
+
+def print_grid(n):
+    
