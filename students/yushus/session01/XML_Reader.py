a340fd8af3bef9bff6c1ed85be74b76eaf7d678a
yushusongmsazure <yushusong@outlook.com>
2018-10-10 20:46:11 -0700

    Save xml reader

--- students/yushus/session01/XML_Reader.py
+++ students/yushus/session01/XML_Reader.py
@@ -5,17 +5,25 @@ def parseXML(xmlFile):
     tree = ET.parse(xmlFile)
 
     root = tree.getroot()
+    total = 0
 
-    group = root.findall('CloudGroup')
+    groups = root.findall('CloudGroup')
+    #clouds = groups.findall('Cloud')
 
-    for item in group:
-        print(item)
+    for item in groups:
+        clouds = item.findall('Cloud')
+        for cloud in clouds:
+            glb = cloud.find('GlobalValues')
+            envs = glb.findall('EnvironmentSetting')
+            for env in envs:
+                name = env.get('Name')
+                match = ['Providers.Feature.Instances','Providers.AppliancePublisher.Instances','Providers.Authorization.Instances','Providers.Shim.Instances','Providers.Test.Instances','Frontdoor.Web.Instances','Frontdoor.Admin.Instances']
+                if name in match:
+                    v = int(env.get('Value'))
+                    total = total + v
+                    print(name)
+                    print(v)
+    print('Total:')
+    print(total)
 
-
-
-
-
-
-
-
-parseXML('Settings_AUX_Prod_Providers.xml')
+parseXML('Settings_AUX_Prod_Frontdoor.xml')
