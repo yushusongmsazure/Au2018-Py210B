939cd55f3d2576af80035f998f05678df9571d70
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-10-12 18:49:55 -0700

    update series.py

--- students/brandon_nguyen/session02/series.py
+++ students/brandon_nguyen/session02/series.py
@@ -11,4 +11,15 @@ def fibonacci(n):
         return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+def lucas(n):
+    """
+    The Lucas Numbers are a related series of integers that start with the values 2 and 1 rather than 0 and 1. 
+    The resulting series looks like this:2, 1, 3, 4, 7, 11, 18, 29, ... similiar to f(n)=f(n-1)+f(n-2)
+    The function lucas returns the nth value in the lucas numbers series (starting with zero index).
+    """
+    if (n==0 or n == 1):
+        return abs(n-2)
+    else:
+        return lucas(n-1) + lucas(n-2)
+        
 
