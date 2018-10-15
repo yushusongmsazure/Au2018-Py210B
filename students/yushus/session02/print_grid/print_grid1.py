da22b4b9e57935b54761eb6f09061ea56c05266c
yushusongmsazure <yushusong@outlook.com>
2018-10-15 12:59:39 -0700

    add spaces

--- students/yushus/session02/print_grid/print_grid1.py
+++ students/yushus/session02/print_grid/print_grid1.py
@@ -7,28 +7,28 @@ def print_grid1(n):
     # control the height or how many rows to print
     for l in range(2):
         # print the first + sign
-        print('+', end='')
+        print('+ ', end='')
 
         # control the length or how many columns to print
         for i in range(2):
             # one iteration looks like: ----+
-            print('-'*unit,end='')
-            print('+',end='')
+            print('- '*unit,end='')
+            print('+ ',end='')
 
         # print the vertical bars |
         # the number of | should be the same number as - as within above for loop
         for a in range(unit):
             print()
-            print('|',end='')
+            print('| ',end='')
             for i in range(2):
-                print(' '*unit,end='')
-                print('|', end='')
+                print('  '*unit,end='')
+                print('| ', end='')
         print()
 
     # print the last line of pattern +----+----+
-    print('+', end='')
+    print('+ ', end='')
     for b in range(2):
-        print('-'*unit,end='')
-        print('+',end='')
+        print('- '*unit,end='')
+        print('+ ',end='')
 
 print_grid1(15)
