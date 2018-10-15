dae2bb32cec52152d3a49aa615c380e11891a1af
yushusongmsazure <yushusong@outlook.com>
2018-10-15 13:34:36 -0700

    address comments

--- students/yushus/session02/series.py
+++ students/yushus/session02/series.py
@@ -17,21 +17,12 @@ def sum_series(n, n0=0, n1=1):
 
     if n0 == 2 and n1 == 1, the result is the Lucas series
     """
-
-    if n0 == 0 and n1 == 1:
-        if n <= 1:
-            return n
-        else:
-            return (sum_series(n-1, n0, n1) + sum_series(n-2, n0, n1))
-    elif n0 == 2 and n1 == 1:
-        if n == 0:
-            return 2
-        elif n == 1:
-            return 1
-        else:
-            return (sum_series(n-1, n0, n1) + sum_series(n-2, n0, n1))
+    if n == 0:
+        return n0
+    elif n == 1:
+        return n1
     else:
-        raise "Function not defined"
+        return (sum_series(n-1, n0, n1) + sum_series(n-2, n0, n1))
 
 if __name__ == "__main__":
     # run some tests
