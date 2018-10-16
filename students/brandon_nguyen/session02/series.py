39a533d5dda10da688df9989d6f2c038085c8069
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-10-15 22:01:35 -0700

    Beta release of Series

--- students/brandon_nguyen/session02/series.py
+++ students/brandon_nguyen/session02/series.py
@@ -1,4 +1,6 @@
+
 #!/usr/bin/env python3
+import unittest
 #Week2 Excercise #2
 #Student: Brandon Nguyen - Au2018
 def fibonacci(n):
@@ -31,15 +33,58 @@ def sum_series(n,n0=0,n1=1):
     elif do other 
 
     """
-    #validate arguments - what is the best way?
-    try:
-         x=(n + n0 + n1)
-            #print("do in try")
-    except NameError as err:
-        print(err)
-        print("Function usage sum_series(n,n0=0,n1=0) n,n0,n1 are int.")
+    #ask the instructor which is the best way to check args.
+    if (n0==0 and n1==1):
+    #do fibonacci
+        if (n==0 or n==1): 
+            return n
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+    elif(n0==2 and n1==1):
+    #do lucas    
+        if (n==0 or n == 1):
+            return abs(n-2)
+        else:
+            return lucas(n-1) + lucas(n-2)
     else:
-        print("do this now if correct value input")
+        print("TODO - Other Series sum to do")
+
+
+#unit test using assert do ask about assertEqual as I am unable to make use of it
+if __name__ == "__main__":
+    #rename functions to save typing
+    f = fibonacci
+    l = lucas
+    s = sum_series
+
+    assert f(0) == 0
+    assert f(1) == 1
+    assert f(2) == 1
+    assert f(3) == 2
+    assert f(4) == 3
+    assert f(5) == 5
+    assert f(6) == 8
+    assert f(7) == 13
+
+    assert l(0) == 2
+    assert l(1) == 1
+
+    assert l(4) == 7
+
+    assert s(5) == f(5)
+
+    # test if sum_series matched lucas
+    assert s(5, 2, 1) == l(5)
+
+    #some control test of not equal
+    assert s(5,2,1) != l(6)
+    assert s(5,2,1) != f(6)
+
+    print(" all tests passed including not equal")
+#rename functions
+
+  
+
 
 
 
