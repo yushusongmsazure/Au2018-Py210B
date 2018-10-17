2cb383359696dd8c8474de660950390535903c1e
Ying Guo <guoguoying2013@gmail.com>
2018-10-16 17:56:10 -0700

    fizzbuzz

--- students/YingGuo/session02/HW2FizzBuzz.py
+++ students/YingGuo/session02/HW2FizzBuzz.py
@@ -1,10 +1,10 @@
-"""Homework 2 #2 requirement:
-Write a program that prints the numbers from 1 to 100 inclusive.
-But for multiples of three print “Fizz” instead of the number.
-For the multiples of five print “Buzz” instead of the number.
-For numbers which are multiples of both three and five print “FizzBuzz” instead."""
+
 
 for i in range(1,101):
+    """This scripy prints the numbers from 1 to 100 inclusive.
+    But for multiples of three print “Fizz” instead of the number.
+    For the multiples of five print “Buzz” instead of the number.
+    For numbers which are multiples of both three and five print “FizzBuzz” instead."""
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     elif i % 5 == 0:
