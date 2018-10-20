31485ea4de692ef46a7d15d222305d65a6ac59fd
motians <motians@comcast.net>
2018-10-20 07:59:25 -0700

    small start, needs work

---
+++ students/shawn_degraw/session03/strformat_lab.py
@@ -0,0 +1,9 @@
+#Task One
+"File_{:03d} :  {:.2f}, {:.2e}, {:.3g}".format(2, 123.4567, 10000, 12345.67)
+
+#Task Two
+(value=2,value2=123.4567)
+
+f"File_{value.zfill(3)} :  {round(value2,2)}, {}, {}"
+
+
