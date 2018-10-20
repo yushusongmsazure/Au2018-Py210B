36fb253f2ffa12b324b9288376ff8fb2bbe41dd9
motians <motians@comcast.net>
2018-10-20 09:39:00 -0700

    added name list, new name and new donation

--- students/shawn_degraw/session03/mailroom.py
+++ students/shawn_degraw/session03/mailroom.py
@@ -1,13 +1,34 @@
 #Mailroom Part 1
 import sys
 
-donor_db == [("John Smith", [500.00, 150.00, 20.00]),
-             ("Jane Doe", [340.00, 30.00, 200.00]),
-             ("Jason Bourne", [240.00, 140.00]),
-             ("")]
+donor_db = [("John Smith", [500.00, 150.00, 20.00]),
+            ("Jane Doe", [340.00, 30.00, 200.00]),
+            ("Jason Bourne", [240.00, 140.00]),
+            ("John Wick", [1000.00]),
+            ("GI Jane", [150.00, 60.00])
+           ]
+def adddonation(donoridx):
+    donationammount = input("Enter donation amount> ")
+    donor_db[donoridx][1].append(float(donationammount))
+
+def printdonorlist():
+    for name in donor_db:
+        print(name[0])
+
+def handlename(namechoice):
+    #getting the name index starting from 1 to use truthiness in if statement
+    nameidx = [idx for idx, nametuple in enumerate(donor_db,1) if nametuple[0] == namechoice]
+    if nameidx:
+        print("Using this person: {}".format(donor_db[nameidx[0]-1]))
+        adddonation(nameidx[0]-1)
+    else:
+        print("Adding name to DB.")
+        donor_db.append((namechoice, []))
+        adddonation(len(donor_db)-1)
 
 def exit_program():
     print("Thank you. Bye")
+    print(donor_db)
     sys.exit()
 
 def main():
@@ -23,9 +44,13 @@ def main():
     while True:
         choice = input(menu)
         if choice == '1':
-            print("Selected 1")
+            name = input("Please enter full name> ")
+            if name == "list":
+                printdonorlist()
+            else:
+                handlename(name)
         elif choice == '2':
-            print("Selected 2")
+            print("Selected 21")
         elif choice == '3':
             exit_program()
         else:
