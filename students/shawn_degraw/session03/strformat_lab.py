2c6e517b6b7d8b5f15305d8727c32e88926cd3b8
motians <motians@comcast.net>
2018-10-20 21:46:22 -0700

    First attempt string format

--- students/shawn_degraw/session03/strformat_lab.py
+++ students/shawn_degraw/session03/strformat_lab.py
@@ -1,9 +1,33 @@
 #Task One
-"File_{:03d} :  {:.2f}, {:.2e}, {:.3g}".format(2, 123.4567, 10000, 12345.67)
+print("Task One")
+print("File_{:03d} :  {:.2f}, {:.2e}, {:.3g}".format(2, 123.4567, 10000, 12345.67))
 
 #Task Two
-(value=2,value2=123.4567)
+print("Task Two")
+#(value=2,value2=123.4567)
 
-f"File_{value.zfill(3)} :  {round(value2,2)}, {}, {}"
+#f"File_{value.zfill(3)} :  {round(value2,2)}, {}, {}"
+
+#Task Three
+print("Task Three")
+#def formatter(in_tuple):
+#    form_string = "the length numbers are: {}"
+#    return form_string.format(in_tuple)
+
+#formatter((2,3,5,7,9))
 
 
+#Task Four
+print("Task Four")
+print("{3:02} {4:02} {2:02} {0:02} {1:02}".format(4, 30, 2017, 2, 27))
+
+#Task Five
+print("Task Five")
+tasklist = ['oranges', 1.3, 'lemons', 1.1]
+
+print(f"The weight of an {tasklist[0][0:-1]} is {tasklist[1]} and the weight of a {tasklist[2][0:-1]} is {tasklist[3]}")
+
+print(f"The weight of an {tasklist[0][0:-1].upper()} is {(tasklist[1] * .2) + tasklist[1]} and the weight of a {tasklist[2][0:-1].upper()} is {(tasklist[3] * .2) + tasklist[3]}")
+
+#Task Six
+print("Task Six")
