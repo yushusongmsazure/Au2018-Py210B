d5c37839b8b3334ccbaa29d869dd6fe0bc738b38
ikemerrixs <kwamorrison@gmail.com>
2018-10-22 18:54:59 -0700

    assignment add

--- students/merrix/session03/slicing.py
+++ students/merrix/session03/slicing.py
@@ -1,16 +1,22 @@
-def exchange_first_last(str_seq, tup_seq):
+def exchange_first_last(str_seq):
     strseq = list(str_seq)
-    ''.join(strseq)
     strseq[0], strseq[-1] = strseq[-1], strseq[0]
-    tup_seq[0], tup_seq[-1] = tup_seq[-1], tup_seq[0]
-    a_new_sequence = (strseq, tup_seq)
-     
+    
+    a_new_sequence = ''.join(strseq)
+
     return a_new_sequence
-    
-    
+
 a_string = ("this is a string")
+
+def exchange_first_last1(tup_seq):
+    tup_seq[0], tup_seq[-1] = tup_seq[-1], tup_seq[0]
+    new_sequence = (tup_seq)
+    return new_sequence
 a_tuple = [2, 54, 13, 12, 5, 32]
 
-assert (exchange_first_last(a_string, a_tuple)) == "ghis is a strint" ()
+assert exchange_first_last1(a_tuple) == [32, 54, 13, 12, 5, 2]
 
+
+
+assert exchange_first_last(a_string) == ("ghis is a strint")
 
