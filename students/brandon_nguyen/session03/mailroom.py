44175d8ed455ff8464c3826a02035c672df82a46
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-10-24 23:36:35 -0700

    final mailroom.py

--- students/brandon_nguyen/session03/mailroom.py
+++ students/brandon_nguyen/session03/mailroom.py
@@ -7,10 +7,10 @@ from decimal import Decimal
 #Student: Brandon Nguyen - Au2018
 
 #global define data structure
-donor_db = [("Brandon Nguyen", [4500,350,300.05]),
-          ("Shawn DeGraw", [5500, 345,571.3]),
-          ("Jacqueline Lee",[4300,3200,230.1]),
-          ("Aidan Nguyen", [4300,200,238.2]),
+donor_db = [("Brandon Nguyen", [4500.33,350.87,300.05]),
+          ("Shawn DeGraw", [5500.00, 345.05,571.33]),
+          ("Jacqueline Lee",[4300.00,3200.00,230.13]),
+          ("Aidan Nguyen", [4300.00,200.00,238.23]),
           ]
 promptTxt = "\n".join(("Welcome to the mailroom program!",
           "Please choose from below 3 options:",
@@ -30,40 +30,45 @@ def main_menu():
           ">>> "))
 
 def send_ty():
-    response = input("\nPlease enter donor full names or \"list\": " )
-    if response.strip() == 'list':
-        list_donors()
-   #elif response.strip() in donor_db:
-    else:
-        print(response.strip())
-        for name in donor_db:
-            #print(name[0])
-            if name[0] == response.strip():
-                indx = donor_db.index(name)
-                donation = float(input("Please enter donation amount for " + name[0] + ": "))
-                donor_db[indx][1].append(donation)
-                print("\n\nHello {},\nThank you so much for the generious donation of {}!\n\n".format(name[0],float(donation)))
-                break
+    while True:
+        response = input("\nPlease enter donor full names or \"list\". Q to return to main menu: " )
+        if response.strip() == 'list':
+            list_donors()
+            break
+        elif response.strip() == 'Q':
+            main()
         else:
-            donor_db.append((response.strip(),[0])) #adding new name with $0 donation.
-    #print(donor_db)       
-
-        
-
-def sort_seq(seq):
-    pass
-
+            for name in donor_db:
+            #print(name[0])
+                if name[0] == response.strip():
+                    indx = donor_db.index(name)
+                    while True:
+                        donation = input("Please enter donation amount for " + name[0] +". Q to quit. :>>")
+                        if donation.strip() == 'Q':
+                            main()
+                        else:
+                            break
+                    donor_db[indx][1].append(float(donation))
+                    print("\n\nHello {},\nThank you so much for the generious donation of {}!\n\n".format(name[0],float(donation)))
+                    main() #go to main menu not ideal way.  Should have another function that does the menu option
+            else:
+                donor_db.append((response.strip(),[0.00])) #adding new name with $0 donation. avoiding div/0 issue
+      
 def create_rpt():
-    newList = sort_sum()
-    header_string = "Donor Name               | Total Given | Num Gifts | Average Gift"
+    """
+    This function is to print the report of donor.  
+    """
+    #Currently need more work on the format as 0.0 problem not yet solved.
+    print()
+    newList = sort_sum() #creating a new list with sorted total in reverse already for easy printing.
+    header_string = "Donor Name              |  Total Given | Num Gifts | Average Gift"
     line = '-'*len(header_string)
     print(header_string)
     print(line)
     for donor, total, num, avg in newList:
-        print(donor, "\t\t", "$  ",total[0], "\t    ",num[0], "\t\t$  {0:.2f}".format(avg[0])   )
-
-
-
+        print( donor, "\t\t$ ",str(total[0]).rjust(13), str(num[0]).rjust(5), "\t   $ ", str(avg[0]).rjust(10) )
+    print("\n\n")
+      
 def sort_sum():
     #creating a new list with computed value for easy printing.
     """
@@ -71,11 +76,13 @@ def sort_sum():
     """
     dbSum = []
     for name, donation in donor_db:
-        dbSum.append((name,[sum(donation)],[len(donation)],[sum(donation)/len(donation)]),)
+        dbSum.append((name,[round(sum(donation),2)],[len(donation)],[round(sum(donation)/len(donation),2)]),)
     return sorted(dbSum, key=lambda donor: donor[1], reverse=True)
     
-
 def list_donors():
+    """
+    Basic Name of donor return in Order by First Name
+    """
     lst = []
     for name in donor_db:
         lst.append(name[0])
