17045ea046d5953f146dda1e3f65a39204173c09
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-10-31 00:57:37 -0700

    add test file for later use

---
+++ students/brandon_nguyen/session04/temp_file_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import unittest
+import tempfile
+from decimal import Decimal
+from datetime import datetime
+
+#test ting tempfile
+
+def creat_tmp_file():
+    #file_name = "Tempfile"+str(datetime.now()).replace(" ","_")+".TXT"
+    file_name = tempfile.TemporaryDirectory()
+
+    #need to study more
+    
