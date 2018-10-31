e10612493108dd9253dd684a17fd55973ac1819b
motians <motians@comcast.net>
2018-10-30 21:46:20 -0700

    Everything working except format with **

--- students/shawn_degraw/session04/mailroomPart2.py
+++ students/shawn_degraw/session04/mailroomPart2.py
@@ -12,15 +12,9 @@ donor_db = {"John Smith": [500.00, 150.00, 20.00],
 
 
 #Thank you letter template
-letterdict = {
-        'tygreeting': 'Dear {name},\n\n',
-        'tybody': 'Thank you for your generous donation of ${value:.2f} to our cause. \nYour donations help keep Python great!\n\n',
-        'tysignature': 'Sincerely\n\nThe Python Project',
-        'generalgreeting': 'Dear {name},\n\n',
-        'generalbody': 'Thank you for your generosity in supporting us with ${value:.2f} in donations. \nWe hope to have your continued support.\n\n',
-        'generalsignature': 'With great thanks\n\nThe Python Project'
-}
-#thankyouletter = "\n".join(("","Dear {name},","","Thank you for your generous donation of ${amount:.2f} to our cause.","Your donations help keep Python great!","","Sincerely","","The Python Project",""))
+thankyouletter = "\n".join(("","Dear {name},","","Thank you for your generous donation of ${amount:.2f} to our cause.","Your donations help keep Python great!","","Sincerely","","The Python Project",""))
+
+generaldonationletter ="\n".join(("","Dear {},","","Thank you for your generosity in supporting us with ${:.2f} in donations.", "We hope to have your continued support.", "","With great thanks", "", "The Python Project"))
 
 def sumdbkey(donorlist):
     """Used by function sortdb to generate sort key
@@ -36,8 +30,7 @@ def printthankyou(donorname):
     """Prints the thank you letter to standard output3
     :param donoridx: the index to the donor in the database the letter should be addressed too
     """
-    print('{0[tygreeting]}{0[tybody]}{0[tysignature]}'.format(letterdict,name=donorname,value=donor_db[donorname][-1:]))
-    #print(thankyouletter.format(name=donorname, amount=donor_db[donorname][-1]))
+    print(thankyouletter.format(name=donorname, amount=donor_db[donorname][-1]))
 
 def adddonation(donorname):
     """Gets donation amount from user and adds it to the donor's entry in db
@@ -83,7 +76,8 @@ def sendletters():
     for donorname in donor_db:
         filename = "letters\\" + donorname.replace(' ', '_') + ".txt"
         with open(filename, 'w') as outfile:
-            outfile.write('{0[generalgreeting]}{0[generalbody]}{0[generalsignature]}'.format(letterdict,name=donorname,value=donor_db[donorname][-1:]))
+            outfile.write(generaldonationletter.format(donorname, sum(donor_db[donorname])))
+    print("\nLetters printed to files.\n")
 
 def exit_program():
     print("Thank you. Bye")
