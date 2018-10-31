07f49d630371d895c63acb55fa1771c7b87c0211
Arun Nalla <nalla.arunkumar@gmail.com>
2018-10-30 23:54:35 -0700

    added Lesson4_Mailroom_Part2.py

---
+++ students/arun_nalla/session04/trigrams.py
@@ -0,0 +1,20 @@
+﻿with open("sherlock_small.txt") as f:
+    words = f.read().strip().replace("\n", " ").split()
+
+def trigram_dict(words):
+	trilist = {}
+	for i in range(len(words)-2):
+    
+		a = ",".join(words[i:i+2])
+    
+		b = words[i+2]    
+		if a not in trilist.keys():
+        
+			trilist[a] = [b]
+    
+		else:
+        
+			trilist[a].append(b)
+
+	return trilist
+
