111e1e2382253e885feddc6b158090f03b260e2a
Jakubiak <Jason.Jakubiak@t-mobile.com>
2018-10-30 22:03:29 -0700

    session04 trigram

--- students/jjakub/session04/trigram.py
+++ students/jjakub/session04/trigram.py
@@ -1,9 +1,11 @@
 
+import sys
 
+### trigram testing
 # words = "I wish I may I wish I might".split()
 # wordcnt = 25
 
-# create dictionary: loop method
+#### create dictionary: loop method
 # d = {}
 # for i in range(len(words)-2):
 #     pair = tuple(words[i:i + 2])
@@ -13,12 +15,35 @@
 #     else:
 #         d[pair].append(follower)
 
-### create dictionary
+def getfile():
+    filename = input("Please provide file path and name: ")
+    # filename = "students//jjakub//session04//irving_sleepy_hollow.txt"
+
+    ### open file
+    f = open(str(filename))
+    book = f.read().replace('\n', ' ')
+
+    ### start and end file text
+    start_str = '*** START OF THIS PROJECT GUTENBERG EBOOK'
+    end_str = '*** END OF THIS PROJECT GUTENBERG EBOOK'
+
+    ### slice based on start and end file text
+    start_indx = book.find(start_str)
+    end_indx = book.find(end_str)
+
+    text = book[start_indx + len(start_str) :end_indx + len(end_str)].split()
+    return text
+
+def trigram_len():
+    n = int(input("Please provide trigram length: "))
+    return n
+
+### create dictionary: setdefault method
 def build_trigrams(words, wordcnt):
     d = {}
     for i in range(len(words)-2):
         pair = tuple(words[i:i + 2])
-        follower = words[i + 2]    
+        follower = words[i + 2]
         d.setdefault(pair,[]).append(follower)
 
     trigrams = {}
@@ -29,22 +54,10 @@ def build_trigrams(words, wordcnt):
         cur_pair = trigrams[-2:]
         if tuple(cur_pair) not in d:
             cur_pair = list(random.choice(list(d.keys())))
-        trigrams.extend(d.setdefault(tuple(cur_pair),[]))
+        trigrams.append(random.choice(d.setdefault(tuple(cur_pair),[])))
 
+    trigrams = ' '.join(trigrams)
     return trigrams
 
 if __name__ == "__main__":
-    trigrams = build_trigrams(words, wordcnt)
-    print(trigrams)
-
-
-filepath = '.\\students\\jjakub\\session04\\'
-filename = 'sherlock_short.txt'
-
-f = open(str(filepath + filename))
-book = f.read()
-f.close()
-
-book = book.split()
-
-build_trigrams(book,wordcnt)
+    build_trigrams(getfile(),trigram_len())
