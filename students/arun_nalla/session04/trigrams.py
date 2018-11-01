18d26c0f42acaab81ebffb8305ae9e4fd54dffd6
Arun Nalla <nalla.arunkumar@gmail.com>
2018-10-31 20:40:57 -0700

    added Trigrams.py

--- students/arun_nalla/session04/trigrams.py
+++ students/arun_nalla/session04/trigrams.py
@@ -1,7 +1,7 @@
 ﻿
 #! usr/bin/env python 3
 
-words = 'I wish I may I wish I might'.split()
+#words = 'I wish I may I wish I might'.split()
 def trigram_dict (words):
     trilist = {}
     for i in range(len(words)-2):
@@ -12,14 +12,17 @@ def trigram_dict (words):
         else:
             trilist[a].append(b)
     return trilist
-
-
+def new_data(old_data):
+    word_list = ''
+    with open('filename.txt', 'r') as file_name:
+        translator = str.maketrans('','', string.punctuation)
+        for lines in file_name.readlines():
+            words = lines.translate(translator)
+            word_list+=words
+    list_of_words = word_list.split()
+    return list_of_words
 import random
-
 import string
-
-
-
 def build_new_story(story):
     keys_first = random.choice(list(trigram_dict(words)))
     #print (keys_first, "key first")
@@ -35,17 +38,8 @@ def build_new_story(story):
             break
         new_story = ' '.join(my_list)
     return (new_story)
-def new_data(file_name):
-    with open("file_name.txt", 'r') as file:
 
-        translator = str.maketrans(',', string.punctuation)
-        for lines in file.readlines():
-            words = lines.translate(translator)
-
-
-    return words
-
-if __name__=='__maim__':
+if __name__=='__main__':
     # get the filename from the command line
     try:
         filename = input ("Enter a file name>>>")
@@ -56,25 +50,3 @@ if __name__=='__maim__':
     word_pairs = trigram_dict(words)
     new_text = build_new_story(word_pairs)
     print (new_text)
-
-
-
-
-"""print(new_text)
-    abc = trigram_dict(words)
-    xyz = build_new_story(abc)
-    print(abc)
-    print(xyz)"""
-
-"""if keys_first in my_list:
-        my_list.append(random.choice(trigram_dict(words)[keys_first]))
-        print (my_list)
-        key_next = tuple(my_list[-2:])
-        print (key_next, "next key")
-        my_list.append(random.choice(trigram_dict(words)[key_next]))
-        print (my_list, "next")
-key_next2 = tuple(my_list[-2:])
-print(key_next2)
-my_list.append(random.choice(trigram_dict(words)[key_next2]))
-print (my_list, "next2")"""
-
