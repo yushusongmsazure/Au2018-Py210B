5304ddc6a1fe6310158f77bf263625e64186f2a0
Tim Meese <timm@Tims-MacBook-Pro.local>
2018-11-28 22:33:50 -0800

    Implemented base of html renderer including indent

--- students/timm67/session07/html_render.py
+++ students/timm67/session07/html_render.py
@@ -7,25 +7,47 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element(object):
-    object.content = []
+    _tag = str('html')
+    _indent = 0
 
     def __init__(self, content=None):
         if content is not None:
-            self.append(content)
+            self._content = [content]
+        else:
+            self._content = []
 
     def append(self, new_content):
-        self.content.append(new_content)
+        if new_content is not None:
+            self._content.append(new_content)
 
-    def render(self, out_file):
-        try:
-            # TODO: may want to append to already opened file (i.e. call render multiple times)
-            with open(out_file, 'w') as fd:
-                for line in self.content:
-                    try:
-                        fd.write(line)
-                    except IOError:
-                        print("I/O Error with file [{0}] on writeline".format(out_file))
-                        return
-        except IOError:
-            print("I/O Error with file [{0}] on open".format(out_file))
-            return
+    def render(self, out_fd):
+        if self._indent > 0:
+            indent = ' ' * self._indent
+        else:
+            indent = ''
+        out_fd.write('{0}<{1}>'.format(indent, self._tag))
+        # loop through the list of contents:
+        for content in self._content:
+            try:
+                out_fd.write('{0}{1}'.format(indent, content))
+                try:
+                    content.render(out_fd)
+                except AttributeError:
+                    out_fd.write(content)
+                out_fd.write('\n')
+            except IOError:
+                print("I/O Error")
+                return
+        out_fd.write('{0}</{1}>'.format(indent, self._tag))
+
+class Html(Element):
+    _tag = str('html')
+    _indent = 0
+
+class Body(Element):
+    _tag = str('body')
+    _indent = 5
+
+class P(Element):
+    _tag = str('p')
+    _indent = 10
