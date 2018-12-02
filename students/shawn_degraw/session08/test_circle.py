971163a541726d690e64e5130285c080aa727a1d
motians <motians@comcast.net>
2018-12-01 18:15:16 -0800

    test circle done through step 5

--- students/shawn_degraw/session08/test_circle.py
+++ students/shawn_degraw/session08/test_circle.py
@@ -8,7 +8,7 @@ import pytest
 from circle import *
 
 
-#Step 1 tests
+# Step 1 tests
 
 def test_radius():
 
@@ -17,24 +17,11 @@ def test_radius():
     assert(c.radius) == 5
 
 
-def test_zeroradius():
+def test_emptycircle():
 
-    try:
-        badc = Circle(0)
-    except ValueError:
-        assert(True)
-    else:
-        assert(False)
+    c = Circle()
 
-
-def test_stringradius():
-
-    try:
-        badc = Circle('test')
-    except TypeError:
-        assert(True)
-    else:
-        assert(False)
+    assert(c.radius) == 0
 
 
 # Step 2 tests
@@ -44,3 +31,35 @@ def test_diameter():
     c = Circle(5)
 
     assert(c.diameter) == 10
+
+
+# Step 3 tests
+
+def test_setdiameter():
+
+    c = Circle()
+    c.diameter = 6
+
+    assert(c.radius) == 3
+    assert(c.diameter) == 6
+
+
+# Step 4 tests
+
+def test_area():
+
+    c = Circle(2)
+
+    assert(c.area) == 12.566371
+
+
+def test_setarea():
+
+    c = Circle(2)
+
+    try:
+        c.area = 12
+    except AttributeError:
+        assert(True)
+    else:
+        assert(False)
