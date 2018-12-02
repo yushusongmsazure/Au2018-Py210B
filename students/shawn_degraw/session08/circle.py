9b58f8593cffd5fe896c94bef1dbb472777185d7
motians <motians@comcast.net>
2018-12-01 18:15:01 -0800

    circle done through step 4

--- students/shawn_degraw/session08/circle.py
+++ students/shawn_degraw/session08/circle.py
@@ -5,17 +5,22 @@ Class to implement simple circle
 
 """
 
+from math import pi
+
 
 class Circle:
 
-    def __init__(self, init_radius):
-        if not init_radius:
-            raise ValueError
-        elif type(init_radius) != int and type(init_radius) != float:
-            raise TypeError
-        else:
+    def __init__(self, init_radius=0):
             self.radius = init_radius
 
     @property
     def diameter(self):
         return 2 * self.radius
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
+
+    @property
+    def area(self):
+        return round(pi * self.radius ** 2, 6)
