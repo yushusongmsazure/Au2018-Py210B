16abd491b462afb6cba02e4e20a3bff8e32999fa
motians <motians@comcast.net>
2018-12-02 10:46:30 -0800

    step 9 complete

---
+++ students/shawn_degraw/session08/sphere.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Class to implement simple sphere
+
+"""
+
+from circle import *
+
+
+class Sphere(Circle):
+
+    @property
+    def volume(self):
+        return round(4 / 3 * pi * self.radius ** 3, 6)
+
+    def __str__(self):
+        return "Sphere with radius: {}".format(self.radius)
+
+    def __repr__(self):
+        return "Sphere({})".format(self.radius)
+
+    @property
+    def area(self):
+        raise NotImplementedError
+
