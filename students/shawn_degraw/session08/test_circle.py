975f6cc02ea0b884e47186e4e388a81f82aa608f
motians <motians@comcast.net>
2018-12-02 13:08:11 -0800

    added object type tests and reverse multiply test

--- students/shawn_degraw/session08/test_circle.py
+++ students/shawn_degraw/session08/test_circle.py
@@ -15,14 +15,14 @@ def test_radius():
 
     c = Circle(5)
 
-    assert(c.radius) == 5
+    assert c.radius == 5
 
 
 def test_emptycircle():
 
     c = Circle()
 
-    assert(c.radius) == 0
+    assert c.radius == 0
 
 
 # Step 2 tests
@@ -31,7 +31,7 @@ def test_diameter():
 
     c = Circle(5)
 
-    assert(c.diameter) == 10
+    assert c.diameter == 10
 
 
 # Step 3 tests
@@ -41,8 +41,8 @@ def test_setdiameter():
     c = Circle()
     c.diameter = 6
 
-    assert(c.radius) == 3
-    assert(c.diameter) == 6
+    assert c.radius == 3
+    assert c.diameter == 6
 
 
 # Step 4 tests
@@ -51,7 +51,7 @@ def test_area():
 
     c = Circle(2)
 
-    assert(c.area) == 12.566371
+    assert c.area == 12.566371
 
 
 def test_setarea():
@@ -72,8 +72,8 @@ def test_diametercontructor():
 
     c = Circle.from_diameter(8)
 
-    assert(c.radius) == 4
-    assert(c.diameter) == 8
+    assert c.radius == 4
+    assert c.diameter == 8
 
 
 # Step 6 tests
@@ -82,10 +82,10 @@ def test_repr():
 
     c = Circle(5)
     d = repr(c)
-    assert(d) == 'Circle(5)'
+    assert d == 'Circle(5)'
 
 
-# Step 7 tests ?
+# Step 7 tests
 
 def test_add():
 
@@ -93,7 +93,7 @@ def test_add():
     c2 = Circle(3)
     c3 = c1 + c2
 
-    assert(c3.radius) == 8
+    assert c3.radius == 8
 
 
 def test_multiply():
@@ -102,11 +102,17 @@ def test_multiply():
 
     c3 = c1 * 3
 
-    assert(c3.radius) == 15
+    assert c3.radius == 15
+    assert isinstance(c3, Circle)
 
-    c4 = 2 * c1
 
-    assert(c3.radius) == 10
+def test_multiply_reverse():
+
+    c2 = Circle(6)
+
+    c4 = 2 * c2
+
+    assert c4.radius == 12
 
 
 # Step 8 tests
@@ -116,8 +122,8 @@ def test_lessthan():
     c1 = Circle(5)
     c2 = Circle(8)
 
-    assert(c1 < c2) == True
-    assert(c2 < c1) == False
+    assert c1 < c2
+    assert not c2 < c1
 
 
 def test_greaterthan():
@@ -125,8 +131,18 @@ def test_greaterthan():
     c1 = Circle(5)
     c2 = Circle(8)
 
-    assert(c1 > c2) == False
-    assert(c2 > c1) == True
+    assert not c1 > c2
+    assert c2 > c1
+
+
+def test_circlesort():
+
+    circles = [Circle(2), Circle(1), Circle(5), Circle(4)]
+
+    circles.sort()
+
+    assert circles == [Circle(1), Circle(2), Circle(4), Circle(5)]
+
 
 # Step 8 optional?
 
@@ -148,3 +164,28 @@ def test_spherediameter():
 
     assert(s.radius) == 4
     assert(s.diameter) == 8
+
+
+def test_spherearea():
+
+    s = Sphere(5)
+
+    try:
+        a = s.area
+    except NotImplementedError:
+        assert(True)
+    else:
+        assert(False)
+
+
+def test_sphereadd():
+
+    s1 = Sphere(5)
+    s2 = Sphere(8)
+    s3 = s1 + s2
+    s4 = s1 * 2
+
+    assert s3.radius == 13
+    assert s4.radius == 10
+    assert type(s3) == Sphere
+    assert type(s3) == Sphere
