daf2eca00f86e85ae01a6ae4fd3f9225d1808a18
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-12-09 15:44:03 -0800

    test 1a all passed

---
+++ students/brandon_nguyen/session07/html_render.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Week7 Excercise html render -
+# Student: Brandon Nguyen - Au2018
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element(object):
+    tag = "html"
+
+    def __init__(self, content=None):
+        self.contents = [content]
+
+    def append(self, new_content):
+        self.contents.append(new_content)
+
+    def render(self, out_file):
+        # out_file.write("just something as a place holder...")
+        out_file.write("<{}>\n".format(self.tag))
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
