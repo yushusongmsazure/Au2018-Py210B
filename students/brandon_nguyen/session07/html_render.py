c57fc1c2ea4ba1773b44b16243b4ca83a7fedcd0
Brandon QM Nguyen <bmnguyen@uw.edu>
2018-12-10 01:30:50 -0800

    update render part5 done

--- students/brandon_nguyen/session07/html_render.py
+++ students/brandon_nguyen/session07/html_render.py
@@ -26,11 +26,12 @@ class Element(object):
         # NEXT LINE replacing with _open_tag()
         #out_file.write("<{}>\n".format(self.tag))  # use in part 3
 
-        #out_file.write(self._open_tag())
+        out_file.write(self._open_tag())
         #out_file.write("\n")
-        out_file.write("<{}".format(self.tag))
-        for k, v in self.element_attrs.items():
-            out_file.write(" {}=\"{}\"".format(k, v))
+        #
+        # out_file.write("<{}".format(self.tag))
+        # for k, v in self.element_attrs.items():
+        #     out_file.write(" {}=\"{}\"".format(k, v))
         out_file.write(">\n")
         for content in self.contents:
             try:
@@ -40,17 +41,20 @@ class Element(object):
                 out_file.write("\n")
         # out_file.write("</{}>\n".format(self.tag))
         out_file.write(self._close_tag())
-
+        #out_file.write("\n")
 
     # python private conv opening tags with items in element_attrs
     def _open_tag(self):
-        pass # TODO
-
+        # TODO
+        open_tag = ["<{}".format(self.tag)]
+        for k, v in self.element_attrs.items():
+            open_tag.append(" {}=\"{}\"".format(k, v))
+        open_tag.append(">")
+        return "".join(open_tag)
 
     # python pivate conv closing tag
     def _close_tag(self):
         return "</{}>\n".format(self.tag)
-        
 
 
 # Create sub-classes of Element
@@ -82,5 +86,29 @@ class OneLineTag(Element):
     def append(self, content):
         raise NotImplementedError
 
+
 class Title(OneLineTag):
     tag = "title"
+
+
+# in STEP 5
+class SelfClosingTag(Element):
+
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError("SelfClosingTag cannot contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, out_file):
+        tag = self._open_tag()[:-1] + " />\n"
+        out_file.write(tag)
+
+    def append(self, *args):
+        raise TypeError("You cannot add content to SelfClosingTag")
+
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+class Br(SelfClosingTag):
+    tag = "br"
